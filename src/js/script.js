1b77e10a58797b21ddc7f4f1824fee0eb3e75395
bartnewk29@gmail.com <bartnewk29@gmail.com>
2020-08-29 12:49:40 +0200

    Init Widget

--- src/js/script.js
+++ src/js/script.js
@@ -242,15 +242,21 @@ class AmountWidget {
 
     console.log("AmountWidget:", thisWidget);
     console.log("constructor arguments:", element);
-    }
   }
   getElements(element) {
     const thisWidget = this;
-  
+
     thisWidget.element = element;
     thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
     thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
     thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
   }
-
+}
+initAmountWidget() {
+  const thisProduct = this;
+  thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+}
+  thisProduct.amountWidgetElem.addEventListener('updated', function () {
+    thisProduct.processOrder();
+  });
 app.init();
